ea98d718afb28f1e13faf04c14726104f09c4356
Sean Tayler <seanjtayler@gmail.com>
2018-08-21 21:26:19 -0700

    basic grid and palette

--- main.js
+++ main.js
@@ -16,14 +16,25 @@
 // grid16 = document.getElementById('grid16')
 
 container = document.getElementById('container')
+palette = document.getElementById('palette')
+colors = document.getElementsByClassName('color')
+
 red = document.getElementById('red')
 blue = document.getElementById('blue')
 green = document.getElementById('green')
 black = document.getElementById('black')
+orange = document.getElementById('orange')
+purple = document.getElementById('purple')
+yellow = document.getElementById('yellow')
+white = document.getElementById('white')
 
-
-
-red.addEventListener('click', (e) => {
-  container.addEventListener('click', (e) => e.target.style.backgroundColor = 'red';)
+palette.addEventListener('click', (e) => {
+  color = e.target.id
+  container.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = color;})
 });
-blue.addEventListener('click', (e) => e.target.style.backgroundColor = 'blue';)
+
+// container.addEventListener('click', (e) => {
+//   console.log(e.target)
+//   e.target.style.backgroundColor = 'red';})
+// blue.addEventListener('click', (e) => e.target.style.backgroundColor = 'blue';)
